e91c12b92486cf6e533257d24ad5174e80eb4555
rathesh77 <rathesh.charles@outlook.fr>
2022-09-29 23:17:38 +0200

    fix timestamp displaying

--- src/helper.js
+++ src/helper.js
@@ -1,19 +1,22 @@
 
 function formatDate(date) {
- /* const {day, month, year} = date
-  const str = [day, month, year].map((e) => e.low < 10 ? '0' + e.low : e.low).join('/')
-  return str
-*/
-  const {low, high} = date
-  let lowBits = (low).toString(2)
-  lowBits = '0'.repeat(32 - lowBits.length).concat(lowBits)
-  let highBits = (high).toString(2)
-  highBits = '0'.repeat(32 - highBits.length).concat(highBits)
 
-  const fullBits = highBits.concat(lowBits)+ ""
+  const { low, high } = date
+  let lowBits = null
+  let highBits = null
+
+  lowBits = (low).toString(2)
+  highBits = (high).toString(2)
+  if (low < 0)
+    lowBits = (parseInt(lowBits.split('').map((b) => b === '1' ? '0' : '1').join(''), 2) + 1).toString(2)
+  if (high < 0)
+    highBits = (parseInt(highBits.split('').map((b) => b === '1' ? '0' : '1').join(''), 2) + 1).toString(2)
+
+  let fullBits = highBits.concat(lowBits) + ""
+  fullBits = '0'.repeat(64 - fullBits.length).concat(fullBits)
   const dateTime = new Date(parseInt(fullBits, 2))
   const day = dateTime.getDate()
-  const month = dateTime.getMonth()
+  const month = dateTime.getMonth() + 1
   const year = dateTime.getFullYear()
   const hours = dateTime.getHours()
   const minutes = dateTime.getMinutes()
