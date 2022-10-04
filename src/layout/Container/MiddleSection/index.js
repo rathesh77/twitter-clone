c3fce46d8934d13c3ea1b41cfb6519ca49df473e
rathesh77 <rathesh.charles@outlook.fr>
2022-10-04 22:11:26 +0200

    remove unused imports

--- src/layout/Container/MiddleSection/index.js
+++ src/layout/Container/MiddleSection/index.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useRef } from 'react';
-import { Outlet, Route, Routes, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Timeline from '../../../pages/Timeline';
 import ViewTweet from '../../../pages/ViewTweet.js';
 import Profile from '../../../pages/Profile';
-import RightSection from '../RightSection/index.js'
 
 export default function MiddleSection() {
   const { search, pathname } = useLocation()
--- src/layout/Container/index.js
+++ src/layout/Container/index.js
@@ -1,13 +1,9 @@
 import { useEffect, useRef } from 'react';
-import { Outlet, Route, Routes, useLocation } from 'react-router-dom';
-import Timeline from '../../pages/Timeline';
-import ViewTweet from '../../pages/ViewTweet.js';
-import Profile from '../../pages/Profile';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import RightSection from './RightSection/index.js'
 import MiddleSection from './MiddleSection';
 
 export default function MainPane() {
-  const { search, pathname } = useLocation()
   return (
     <Routes>
       <Route path="/*" element={<Container />}/>
