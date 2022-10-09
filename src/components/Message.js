fb53f5fbeec9551c184fcb00b30d0a8d43f6725d
rathesh77 <rathesh.charles@outlook.fr>
2022-10-10 01:01:20 +0200

    improved chats UI

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,8 @@
+import { Avatar } from "@mui/material";
 import { blue, grey } from "@mui/material/colors";
 import { useContext, useState } from "react";
 import AuthContext from "../authContext";
-import {formatMillisecondsToDate} from '../helper'
+import { formatMillisecondsToDate } from '../helper'
 
 export default function Message(props) {
   const authContext = useContext(AuthContext)
@@ -12,32 +13,34 @@ export default function Message(props) {
   const messageStyles = {
     'me': {
       justifyContent: 'flex-end',
-      boxColor: blue[600],
+      boxColor: blue[300],
       color: 'white',
 
     },
-    'their':{
+    'their': {
       justifyContent: 'flex-start',
-      boxColor:  blue[50],
+      boxColor: grey[300],
       color: 'black',
 
     }
   }
   return (
-    <div className="message-wrapper" style={{justifyContent: messageStyles[identity].justifyContent}}>
+    <div className="message-wrapper" style={{ justifyContent: messageStyles[identity].justifyContent }}>
       <div>
-      {author.uid == authContext.user.uid ? null : (
-              <div className="message-header">
-                <div className="message-author-tag">{author.username}</div>
-              </div>
-            )}
-        <div className="message-content" style={{backgroundColor: messageStyles[identity].boxColor, color: messageStyles[identity].color}}>
-          <div
-            className="message-text"
-            dangerouslySetInnerHTML={{ __html: message }}
-          ></div>
+        {author.uid == authContext.user.uid ? <div></div> : (
+          <div className="message-header">
+            <div className="message-author-tag"><Avatar src={author.avatar} /></div>
+          </div>
+        )}
+        <div>
+          <div style={{ justifyContent: messageStyles[identity].justifyContent }}>
+            <div className="message-content" style={{ backgroundColor: messageStyles[identity].boxColor, color: messageStyles[identity].color }} dangerouslySetInnerHTML={{ __html: message }}>
+            </div>
+          </div>
+
+          <div className="message-timestamp">{date}</div>
         </div>
-        <div className="message-timestamp">{date}</div>
+
 
       </div>
     </div>
