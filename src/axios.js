7d0910a6ba0adfd9f4fc3d1e2c779931879427fb
rathesh77 <rathesh.charles@outlook.fr>
2023-11-06 10:40:23 +0100

    add REACT_APP_BACKEND_PORT env variable and setup axios baseURL accordingly

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:8080'
+  baseURL: 'http://'+window.location.host.split(':')[0] + ':' + process.env['REACT_APP_BACKEND_PORT']
 })
 
 export {instance as axiosInstance}
